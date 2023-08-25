bde9f172d4ea60b26db3852045258093b85e7427
Pablo Rodrigues <115594757+PabloR18@users.noreply.github.com>
2023-08-25 17:27:27 -0300

    25/08/2023

--- AlistamentoMilitar.java
+++ AlistamentoMilitar.java
@@ -18,6 +18,10 @@ public class AlistamentoMilitar {
 		this.alistamentoMilitar[7] = peso;
 	}
 	
+	public String[] getAlistamentoMilitar() {
+		return alistamentoMilitar;
+	}
+	
 	public void informarIdade(Scanner entrada) {
 		
 		System.out.println("-- Alistamento Militar --\n");
@@ -68,28 +72,28 @@ public class AlistamentoMilitar {
 	}
 	
 	public void exibirInformacoes() {
-		if(getAnoNascimento() >= 20) {
+		if(getAlistamentoMilitar()[0] >= 20) {
 			System.out.println("---------------------------------");
-			System.out.println("Nome: " + getNome());
-			System.out.println("Ano Nascimento: " + (2023 - getAnoNascimento()));
-			System.out.println("Nome da Mãe: " + getNomeDaMae());
-			System.out.println("Nome do Pai: " + getNomeDoPai());
-			System.out.println("Telefone: " + getTelefone());
-			System.out.println("E-mail: " + getEmail());
-			System.out.println("Altura: " + getAltura());
-			System.out.println("Peso: " + getPeso() + "KG");
-			System.out.println("Atenção, Taxa a ser paga R$" + (taxaAlistamento * getAnoNascimento()));
+			System.out.println("Nome: " + getAlistamentoMilitar()[1]);
+			System.out.println("Ano Nascimento: " + (2023 - getAlistamentoMilitar()[0]));
+			System.out.println("Nome do Pai: " + getAlistamentoMilitar()[2]);
+			System.out.println("Nome da Mãe: " + getAlistamentoMilitar()[3]);
+			System.out.println("Telefone: " + getAlistamentoMilitar()[4]);
+			System.out.println("E-mail: " + getAlistamentoMilitar()[5]);
+			System.out.println("Altura: " + getAlistamentoMilitar()[6]);
+			System.out.println("Peso: " + getAlistamentoMilitar()[7] + "KG");
+			System.out.println("Atenção, Taxa a ser paga R$" + (taxaAlistamento * getAlistamentoMilitar()[0]));
 			System.out.println("---------------------------------");
 		}else {
 			System.out.println("---------------------------------");
-			System.out.println("Nome: " + getNome());
-			System.out.println("Ano Nascimento: " + (2023 - getAnoNascimento()));
-			System.out.println("Nome da Mãe: " + getNomeDaMae());
-			System.out.println("Nome do Pai: " + getNomeDoPai());
-			System.out.println("Telefone: " + getTelefone());
-			System.out.println("E-mail: " + getEmail());
-			System.out.println("Altura: " + getAltura());
-			System.out.println("Peso: " + getPeso() + "KG");
+			System.out.println("Nome: " + getAlistamentoMilitar()[1]);
+			System.out.println("Ano Nascimento: " + (2023 - getAlistamentoMilitar()[0]));
+			System.out.println("Nome do Pai: " + getAlistamentoMilitar()[2]);
+			System.out.println("Nome da Mãe: " + getAlistamentoMilitar()[3]);
+			System.out.println("Telefone: " + getAlistamentoMilitar()[4]);
+			System.out.println("E-mail: " + getAlistamentoMilitar()[5]);
+			System.out.println("Altura: " + getAlistamentoMilitar()[6]);
+			System.out.println("Peso: " + getAlistamentoMilitar()[7] + "KG");
 			System.out.println("---------------------------------");
 		}
 		
